54e9aa6eb9dd0ad913b9c47f5deeed4f8cb2238a
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2022-06-30 15:20:13 +0530

    Add cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
 app.use(express.json());
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Server started running on http://localhost:${PORT}`);
