2b0532fea5f31cf90f4c22a69637827678e6e069
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2022-06-30 15:20:25 +0530

    Add error handling

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
+// Error handling
+app.use((err, req, res, next) => {
+    if (!err) next();
+    const status = err.statusCode || 500;
+    let payload = { message: err.message };
+    if (process.env.DEBUG === "true")
+        payload = { ...payload, stack: err.stack };
+    res.status(status).send(payload);
+});
+
 app.listen(PORT, () => {
     console.log(`Server started running on http://localhost:${PORT}`);
 });
